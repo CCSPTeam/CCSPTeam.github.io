d30940425831cb4cc066f46097f2b9c1c9dd696a
Pierre <cabanispcg@gmail.com>
2019-12-02 14:24:00 +0100

    [ADD] ajout des fichiers de base

---
+++ src/script.js
@@ -0,0 +1,6 @@
+// Load file
+// var svg = d3.xml('../SVGs/PoC.svg')
+//   .then(data => {
+//        d3.select('div.display').node().append(data.documentElement)
+//        console.log(data);
+//    })
