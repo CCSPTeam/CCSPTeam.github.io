897d0cec757e917d8769fd28cbce3c9c819b4df5
sj777 <steeven.janny@etu-univ.lyon1.fr>
2019-12-03 20:29:22 +0100

    Demo onmouseover sur un élèment du svg

--- src/script.js
+++ src/script.js
@@ -4,3 +4,15 @@
 //        d3.select('div.display').node().append(data.documentElement)
 //        console.log(data);
 //    })
+
+var svg = d3.select("#svg-container");
+
+d3.xml("../SVGs/PoC.svg")
+    .then(data => {
+        svg.node().append(data.documentElement)
+        var test = svg.select("g#Bass")
+            .on("mouseover", function () {
+                console.log("toto");
+            })
+        console.log(test)
+    });
