b7c1e304b04154f703815363c491613790539941
sj777 <steeven.janny@etu.univ-lyon1.fr>
2020-01-08 14:56:41 +0100

    Update on dimension Management

--- script_histogram.js
+++ script_histogram.js
@@ -24,7 +24,7 @@ function updateHistogram(data, deviceName){
         .append("svg")
         .attr("id","svg_histogram")
         .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 -10 500 700")
+        .attr("viewBox", "0 -10 500 500")
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
