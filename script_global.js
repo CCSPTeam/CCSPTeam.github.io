63ff16276d9f7a579c1f7130cd2ca9c3bef8ecec
SteevenJ7 <sj777@orange.fr>
2020-01-14 23:10:34 +0100

    Map data visualisation [DONE]

--- script_global.js
+++ script_global.js
@@ -5,13 +5,24 @@ const projection = d3.geoMercator()
 
 path.projection(projection);
 
-const svg4 = d3.select('#div_global').append("svg")
+var btn_width = (400-8)/4;
+var btn_height = 15;
+
+const svg_map = d3.select('#div_global').append("svg")
     .attr("id", "svg3")
     .attr('preserveAspectRatio', 'xMinYMin meet')
     .attr(
         'viewBox',
         '0 0 500 350'
     )
+
+var svg_btn = d3.select("#div_global_button").append("svg")
+    .attr("id", "svg3")
+    .attr('preserveAspectRatio', 'xMinYMin meet')
+    .attr(
+        'viewBox',
+        '0 0 400 100'
+    );
 
 var titles = ["Total energy production (Mtoe)",
     "Total energy consumption (Mtoe)",
@@ -22,60 +33,49 @@ var titles = ["Total energy production (Mtoe)",
     "Share of wind and solar in electricity production  (%)",
     "CO2 emissions from fuel combustion (MtCO2)"]
 
+// Tooltip sur la map
 var tool = d3.select('body').append('div')
     .attr('class', 'hidden tooltip');
 
-var selected = 0;
+// Tooltip sur les btns
+var selector = svg_btn
+    .append("rect")
+    .attr('width',btn_width)
+    .attr("height", 2)
+    .attr("fill", "#43a10f")
+    .attr("opacity", "0");
 
-const deps = svg4.append("g");
+
+
 d3.csv("Data/enerdata.csv").then(function(data){
     d3.json('Data/countries.geo.json').then(function(geojson) {
+        // Link Datas to geoJSON
         for (var i = 0; i < data.length; i++) {
             for (var j = 0; j < geojson.features.length; j++) {
                 if (data[i].country == geojson.features[j].properties.name) {
                     geojson.features[j].properties.values = Object.values(data[i]);
-                    cpt = cpt +1;
                 }
             }
         }
 
-        var svg_btn = d3.select("#div_global_button").append("svg")
-            .attr("id", "svg3")
-            .attr('preserveAspectRatio', 'xMinYMin meet')
-            .attr(
-                'viewBox',
-                '0 0 400 100'
-            );
+        var carte = svg_map.append("g").selectAll("path")
+            .data(geojson.features);
 
-
+        title = svg_map.append("text")
+            .attr("x", 250)
+            .attr("y", 10)
+            .attr("text-anchor", "middle");
         draw(1)
 
 
         function draw(index) {
-            var max = d3.max(geojson.features, function (d) {
-                if (d.properties.values) {
-                    return parseInt(d.properties.values[index]);
-                }
-            });
-            var min = d3.min(geojson.features, function (d) {
-                if (d.properties.values) {
-                    return parseInt(d.properties.values[index]);
-                } else {
-                    return null;
-                }
-            });
+            [max, min] = get_min_max(index, geojson.features)
 
             var colorScale = d3.scaleQuantize()
                 .domain([ min, max ])
                 .range(["#2166ac", "#4393c3", "#92c5de", "#d1e5f0", "#fddbc7", "#f4a582", "#d6604d", "#b2182b"]);
 
-
-            console.log(d3.schemeRdBu[9])
-
-            console.log(parseInt(max), min)
-            deps.selectAll("path")
-                .data(geojson.features)
-                .enter()
+            carte.enter()
                 .append("path")
                 .attr("d", path)
                 .style("fill", function (d) {
@@ -96,70 +96,38 @@ d3.csv("Data/enerdata.csv").then(function(data){
                 .on('mouseout', function() {
                     tool.classed('hidden', true);
                 });
-            svg4.append("text")
-                .text(titles[index-1])
-                .attr("x", 250)
-                .attr("y", 10)
-                .attr("text-anchor", "middle");
 
+            title.text(titles[index-1]);
 
-            svg4.append("g")
-                .attr("class", "legendQuant")
-                .attr("transform", "translate(400, 10)");
+            update_legend(colorScale);
 
-
-
-            var legend = d3.legendColor()
-                .labelFormat(d3.format(".0f"))
-                .scale(colorScale)
-                .shapePadding(5)
-                .shapeWidth(10)
-                .shapeHeight(5)
-                .labelOffset(12);
-
-            svg4.select(".legendQuant")
-                .call(legend)
+            selector.attr("opacity", "100%")
+                .attr("x", function(d,i){ return get_x_btn(d, index-1)})
+                .attr("y", function(d,i){ return get_y_btn(d,index-1) -2})
 
             svg_btn.append("g")
                 .selectAll("rect")
                 .data(titles)
                 .enter()
                 .append("rect")
-                .attr("x", function(d,i){
-                    if (i<4){
-                        return i*((400-40)/4 +  10) + 5;
-                    }
-                    else{
-                        return (i-4)*((400-40)/4 +  10) + 5;
-                    }
-                })
-                .attr("y", function (d, i) {
-                    if(i<4){
-                        return 5;
-                    }
-                    else{
-                        return 40;
-                    }
-
-                })
-                .attr("width", (400-40)/4)
-                .attr("height", 25)
+                .attr("x", get_x_btn)
+                .attr("y", get_y_btn)
+                .attr("width", btn_width)
+                .attr("height", btn_height)
                 .attr("fill", function(d,i){
                     if (i == index-1){
-                        return "#888888";
-                    }
-                    else{
                         return "#FFFFFF";
                     }
+                    else{
+                        return "#e3e6eb";
+                    }
                 })
-                .attr("stroke", "#000000")
-                .attr("stroke-width", "0.5")
                 .attr("value", function (d,i) {return i})
-                .on("mouseover", function(d, i){
-                    d3.select(this).attr("stroke-width", "2");
+                .on("mousemove", function(d, i){
+                    d3.select(this).attr("fill", "#FFFFFF");
                 })
                 .on("mouseout", function(d,i){
-                    d3.select(this).attr("stroke-width", "0.5");
+                    d3.select(this).attr("fill", "#e3e6eb");
                 })
                 .on("click", function (d, i) {
                     draw(i+1);
@@ -170,29 +138,65 @@ d3.csv("Data/enerdata.csv").then(function(data){
                 .data(titles)
                 .enter()
                 .append("text")
-                .text(function(d){ console.log(d); return d})
-                .attr("x", function(d,i){
-                    if (i<4){
-                        return i*((400-40)/4 +  10) + 50;
-                    }
-                    else{
-                        return (i-4)*((400-40)/4 +  10) + 50;
-                    }
-                } )
-                .attr("y", function (d, i) {
-                    if(i<4){
-                        return 20;
-                    }
-                    else{
-                        return 55;
-                    }
-
-                })
+                .text(function(d){ return d})
+                .attr("x", function (d,i){ return get_x_btn(d,i) + btn_width/2})
+                .attr("y", function (d,i){ return get_y_btn(d,i) + btn_height/2})
                 .style("font-size",  "4px")
                 .attr("text-anchor", "middle");
         }
     });
 });
 
+function get_x_btn(d, i){
+    if (i<4){
+        return i*(btn_width +  2) + 2;
+    }
+    else{
+        return (i-4)*(btn_width +  2) + 2;
+    }
+}
+
+function get_y_btn(d,i){
+    if(i<4){
+        return 5;
+    }
+    else{
+        return 5+btn_height+2;
+    }
+
+}
+
+function get_min_max(index, data){
+    var max = d3.max(data, function (d) {
+        if (d.properties.values) {
+            return parseInt(d.properties.values[index]);
+        }
+    });
+    var min = d3.min(data, function (d) {
+        if (d.properties.values) {
+            return parseInt(d.properties.values[index]);
+        } else {
+            return null;
+        }
+    });
+
+    return [max, min];
+}
 
 
+function update_legend(colorScale){
+    svg_map.append("g")
+        .attr("class", "legendQuant")
+        .attr("transform", "translate(400, 15)");
+
+    var legend = d3.legendColor()
+        .labelFormat(d3.format(".0f"))
+        .scale(colorScale)
+        .shapePadding(5)
+        .shapeWidth(10)
+        .shapeHeight(5)
+        .labelOffset(12);
+
+    svg_map.select(".legendQuant")
+        .call(legend)
+}
